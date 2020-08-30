22de00dc31826a5083cd20b6d20fa1d984497fb7
GuyYaffeErmoza <guyyaffear@gmail.com>
2020-08-30 15:23:54 +0300

    working on assiamnet

--- esayway/useres-service/src/server/routes.js
+++ esayway/useres-service/src/server/routes.js
@@ -1,17 +1,19 @@
-// import { User } from "#root/db/models";
-// import {generateUUID} from "#root/helpers/generateUUID"
-// const setupRoutes = app => {
-//     app.post("/users", async (req,res,next) =>{
-//         if(!req.body.email || !req.body.password){
-//             return next(new Error("Invalid body!"));
-//         }
-//         try {
-//             const newUser = await User.create({
-//                 email:req.body.email,
-//                 id: generateUUID(),
-//                 passwordHash:hashPassword(req.body.passwor)
-//             })
-//         }
-//     })
-// };
-// export default setupRoutes;
+import { User } from "#root/db/models";
+import { generateUUID } from "#root/helpers/generateUUID"
+const setupRoutes = app => {
+    app.post("/users", async (req,res,next) =>{
+        if(!req.body.email || !req.body.password){
+            return next(new Error("Invalid body!"));
+        }
+        try {
+            const newUser = await User.create({
+                email:req.body.email,
+                id: generateUUID(),
+                passwordHash:hashPassword(req.body.passwor)
+            })
+        }catch(e){    
+          return next(e);
+        }
+    })
+};
+export default setupRoutes;
